4b214a3648285b1f50057779ef55318a0192da2a
marcelldac <dacmarcell@gmail.com>
2023-12-02 14:47:31 -0300

    chore: better error handling and code organization

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import * as express from "express";
 import * as cors from "cors";
 
-const PRICE_API_URL = "https://api.coinbase.com/v2/exchange-rates?currency=ETH";
+const COINBASE_API_URL =
+  "https://api.coinbase.com/v2/exchange-rates?currency=ETH";
 const HOST = "localhost";
 const PORT = process.env.PORT || 3000;
 
@@ -13,38 +14,24 @@ app.use(express.json());
 app.post(
   "/api/v1/transaction",
   async (request: express.Request, response: express.Response) => {
-    const { companyBidValue } = request.body;
-    const { rangeBidValue } = request.body;
+    const { rangeBidValue } = request.body; /* 1000-2000 */
+    const rangeSplit = rangeBidValue.split("-");
+    const firstElement = rangeSplit[0];
+    const secondElement = rangeSplit[1];
+
     try {
-      const ETHPrice = await fetch(PRICE_API_URL);
+      const ETHPrice = await fetch(COINBASE_API_URL);
       const { data } = await ETHPrice.json();
       const intETHValue = parseInt(data.rates.BRL);
-      const intBidValue = parseInt(companyBidValue);
 
-      if (companyBidValue) {
-        if (intETHValue == intBidValue) {
-          /* Fazer transação */
-          return response.status(200).json(intETHValue);
-        }
+      if (firstElement <= intETHValue && intETHValue <= secondElement) {
+        return response.sendStatus(202);
       }
 
-      if (rangeBidValue) {
-        const rangeSplit = rangeBidValue.split("-");
-        const firstElement = rangeSplit[0];
-        const secondElement = rangeSplit[1];
-
-        if (firstElement <= intETHValue && intETHValue <= secondElement) {
-          /* Fazer transação */
-          return response
-            .status(200)
-            .json({ message: `Actual ETH value: ${intETHValue}` });
-        }
-      }
-
-      return response.status(406).json(false);
+      return response.status(406).json(intETHValue);
     } catch (error) {
       console.error(error);
-      return response.status(500).json({ message: "Error on fetching api" });
+      return response.status(500).json({ message: "Error on fetching data" });
     }
   }
 );
