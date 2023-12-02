00f4b292c0d305d1500908836fd867cbcc981f73
marcelldac <dacmarcell@gmail.com>
2023-12-02 16:37:37 -0300

    refactor: use transaction service

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -1,7 +1,6 @@
 import * as express from "express";
 
-const COINBASE_API_URL =
-  "https://api.coinbase.com/v2/exchange-rates?currency=ETH";
+import { validateTransaction } from "../services/transaction-service";
 
 //#region Create Transaction
 
@@ -25,26 +24,22 @@ export const create = async (
   response: express.Response
 ) => {
   const { rangeBidValue } = request.body; /* 1000-2000 */
-  const rangeSplit = rangeBidValue.split("-");
-  const firstElement = rangeSplit[0];
-  const secondElement = rangeSplit[1];
 
   try {
-    const ETHPrice = await fetch(COINBASE_API_URL);
-    const { data } = await ETHPrice.json();
-    const intETHValue = parseInt(data.rates.BRL);
+    const { isValid, intETHValue } = await validateTransaction(rangeBidValue);
 
-    if (firstElement <= intETHValue && intETHValue <= secondElement) {
+    if (isValid) {
       return response.sendStatus(202);
     }
 
-    return response.status(406).json(intETHValue);
+    return response
+      .status(406)
+      .json({ message: `Transaction not valid. ETH Value: ${intETHValue}` });
   } catch (error) {
     console.error(error);
     return response.status(500).json({ message: "Error on fetching data" });
   }
 };
-
 //#endregion
 
 const transaction = {
