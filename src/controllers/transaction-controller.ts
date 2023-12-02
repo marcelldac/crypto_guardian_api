82e9bbe491e0ac570bed1d4b33fa68b3a15fe079
marcelldac <dacmarcell@gmail.com>
2023-12-02 16:09:39 -0300

    style: add tsdocs

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -5,6 +5,21 @@ const COINBASE_API_URL =
 
 //#region Create Transaction
 
+/**
+ * The function takes a range bid value, fetches the current ETH price, and returns a response based on
+ * whether the ETH price falls within the specified range.
+ * @param request - The 'request' parameter is an object that represents the HTTP request made to the
+ * server. It contains information such as the request headers, request body, request method, request
+ * URL, etc.
+ * @param response - The 'response' parameter is an object representing the HTTP response that will be
+ * sent back to the client. It is an instance of the 'express.Response' class, which provides methods
+ * for sending the response, such as 'send', 'json', and 'status'.
+ * @returns The function 'create' returns a response to the client. If firstElement is less than
+ * intETHValue and intETHValue is less than secondElement' is true, it returns a status code of 202 (Accepted).
+ * Otherwise, it returns a status code of 406 (Not Acceptable) along with the value of 'intETHValue' in
+ * the response body. If there is an error during the process, an appropriate error message is returned.
+ */
+
 export const create = async (
   request: express.Request,
   response: express.Response
--- src/routes/transaction-router.ts
+++ src/routes/transaction-router.ts
@@ -4,6 +4,9 @@ import transaction from "../controllers/transaction-controller";
 
 const transactionRouter = express.Router();
 
+/* `transactionRouter.use(express.json());` is setting up the middleware for parsing JSON data in the
+request body. */
+
 transactionRouter.use(express.json());
 
 transactionRouter.post("/transaction", transaction.create);
