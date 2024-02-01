3126b49f747fbec1e66b47ea92e9130dc68ec878
marcelldac <dacmarcell@gmail.com>
2024-02-01 11:00:42 -0300

    refactor: removes constants and add utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,8 @@
 export const COINBASE_API_URL =
   "https://api.coinbase.com/v2/exchange-rates?currency=ETH";
+
+export const statusCodes = {
+  Ok: 200,
+  Accepted: 202,
+  InternalServerError: 500,
+};
