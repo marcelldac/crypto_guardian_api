bfaea24b47c77a14c699c01655d390a5006fc618
marcelldac <dacmarcell@gmail.com>
2024-02-01 10:47:30 -0300

    refactor: remove magic number (status codes)

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -3,6 +3,11 @@ import { TransactionService } from "../services/transaction-service";
 import { COINBASE_API_URL } from "../constants/index";
 export class TransactionController {
   private transactionService: TransactionService;
+  private statusCodes = {
+    Ok: 200,
+    Accepted: 202,
+    InternalServerError: 500,
+  };
   constructor() {
     this.transactionService = new TransactionService();
   }
@@ -15,10 +20,14 @@ export class TransactionController {
       const isTransactionValid =
         await this.transactionService.validateTransaction(rangeBidValue);
       if (isTransactionValid)
-        return response.status(202).json({ message: "valid", error: false });
+        return response
+          .status(this.statusCodes.Accepted)
+          .json({ message: "valid", error: false });
       return response.send({ message: "invalid", error: false });
     } catch (error) {
-      return response.status(500).json({ message: error, error: true });
+      return response
+        .status(this.statusCodes.InternalServerError)
+        .json({ message: error, error: true });
     }
   };
   public read = async (_: express.Request, response: express.Response) => {
@@ -30,7 +39,9 @@ export class TransactionController {
         .status(200)
         .json({ message: `BRL-ETH: ${BRLEthereumPrice}`, error: false });
     } catch (error) {
-      return response.status(500).json({ message: error, error: true });
+      return response
+        .status(this.statusCodes.InternalServerError)
+        .json({ message: error, error: true });
     }
   };
 }
