0456e939036edba7ffb988a1c93f423e9250b287
marcelldac <dacmarcell@gmail.com>
2024-02-01 10:32:55 -0300

    test: should separate bid values

--- src/services/transaction-service.test.ts
+++ src/services/transaction-service.test.ts
@@ -9,4 +9,12 @@ describe("Transaction Service", () => {
     }
     expect(isValid).toBe(true);
   });
+  it("should separate bid values", () => {
+    const rangeBidValue = "10000-20000";
+    const rangeSplit = rangeBidValue.split("-");
+    const firstElement = parseInt(rangeSplit[0]);
+    const secondElement = parseInt(rangeSplit[1]);
+    expect(firstElement).toBe(10000);
+    expect(secondElement).toBe(20000);
+  });
 });
