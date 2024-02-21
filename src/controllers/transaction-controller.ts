64a4c21eaafd9a9f068d65d8a342f991200c51ba
dacmarcell <devmarcell@protonmail.com>
2024-02-20 23:08:17 -0300

    style: more lines

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -12,17 +12,21 @@ export default interface ITransactionController {
 
 export class TransactionController implements ITransactionController {
   private transactionService: TransactionService;
+
   constructor() {
     this.transactionService = new TransactionService();
   }
+
   validateTransaction = async (
     request: express.Request,
     response: express.Response
   ) => {
     try {
       const { rangeBidValue } = request.body;
+
       const isTransactionValid =
         await this.transactionService.validateTransaction(rangeBidValue);
+
       if (isTransactionValid) {
         return response
           .status(statusCodes.Accepted)
@@ -38,9 +42,11 @@ export class TransactionController implements ITransactionController {
         .json({ message: error, error: true });
     }
   };
+
   sendBRLPrice = async (_: express.Request, response: express.Response) => {
     try {
       const price = await this.transactionService.getBRLPrice();
+
       return response
         .status(statusCodes.Ok)
         .json({ message: `BRL-ETH: ${price.toFixed(2)}`, error: false });
